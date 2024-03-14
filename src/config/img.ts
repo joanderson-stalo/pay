92a0d6301ef1381f3e0d86982d5e14657a011e88
Joanderson <jhonny_040996@hotmail.com>
2024-03-14 02:41:19 -0300

    fix

--- src/config/img.ts
+++ src/config/img.ts
@@ -1,12 +1,10 @@
 import Background from '@assets/deixanoazul.png'
-import deixaNoAzul from '@assets/deixanoAzulLogoAzul.png'
 import backModal from '@assets/backgroundModal.png'
 import sucesso from '@assets/sucesso.png'
 
 export const ThemeImg = {
   backgroundLogin : Background,
   backModal: backModal,
-  deixaNoAzul: deixaNoAzul,
  
   sucesso: sucesso
 }
