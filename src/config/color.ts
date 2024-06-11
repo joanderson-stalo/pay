169bfced08771f739cb6aa7507dba75e987021a0
Joanderson <jhonny_040996@hotmail.com>
2024-06-11 11:34:12 -0300

    fix

--- src/config/color.ts
+++ src/config/color.ts
@@ -3,9 +3,9 @@
 
 const currentHost = window.location.hostname;
 const segment = currentHost.split('.')[1];
-// export const baseURL = `https://api-${segment}-confrapag.stalopay.com.br/`;
+export const baseURL = `https://api-${segment}-confrapag.stalopay.com.br/`;
 
-export const baseURL = 'https://api-pagueassim-confrapag.stalopay.com.br/'
+// export const baseURL = 'https://api-pagueassim-confrapag.stalopay.com.br/'
 
 
 
