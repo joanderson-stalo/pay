06a420214d3e41241c9ebb586339a7eeae4b6abf
Joanderson <jhonny_040996@hotmail.com>
2024-06-12 15:37:57 -0300

    fix

--- src/pages/Extract/extract.tsx
+++ src/pages/Extract/extract.tsx
@@ -1,20 +1,18 @@
 
 import * as S from './styled';
 import { useEffect, useState } from 'react';
-import { FunnelSimple } from '@phosphor-icons/react';
 import { useLogin } from '@/context/user.login';
 import axios from 'axios';
 import { Loading } from '@/components/Loading/loading';
-import { useFilterLicensed } from './hooks/useFilterLicensed';
-import { EditableButton } from './components/ButtonEdit/buttonEdit';
 import { CardInfo } from '../../components/CardInfo/cardInfo';
-import { ModalExtract } from './components/ModalExtract/modalExtract';
 import { HeaderExtract } from './components/HeaderExtract/headerExtract';
 import { TableExtract } from './components/TableExtract/tableExtract';
 import { ExtractCardMobile } from './Mobile/ExtractCardMobile/extractCardMobile';
-import { TotalBtn } from '@/components/TotalBtn/totalBtn';
-import { BtnFilter } from '@/components/BtnFilter/btnFilter';
 import { baseURL } from '@/config/color';
+import { BtnFilterModal } from '@/components/BtnFilterModal/btnFilterModal';
+import { CustomInput } from '@/components/Input/input';
+import { useTenantData } from '@/context';
+import { TagFilter } from '@/components/TagFilter/tagFilter';
 
 export function Extract() {
   const [filter, setFilter] = useState(false);
@@ -25,9 +23,12 @@ export function Extract() {
   const [inflowsToday, setInflowsToday] = useState<number>(0);
   const [outflowsToday, setOutflowsToday] = useState<number>(0);
   const [statement, setStatement] = useState<any>({});
-  const { state } = useFilterLicensed();
   const { dataUser } = useLogin();
- 
+  const [startDate, setStartDate] = useState<string>('')
+  const [endDate, setEndDate] = useState<string>('')
+
+  const tenantData = useTenantData()
+
   const fetchData = async () => {
     setLoading(true);
     let apiUrl = `${baseURL}getStatementLA`;
@@ -41,7 +42,7 @@ export function Extract() {
     if (endDate && endDate !== 'undefined') {
       apiUrl += `&endDate=${endDate}`;
     }
-   
+
     try {
       const response = await axios.get(apiUrl, {
         headers: {
@@ -49,7 +50,7 @@ export function Extract() {
           'Authorization': `Bearer ${dataUser?.token}`
         }
       });
-      
+
       const { total_in_account, total_inflows, total_outflows, inflows_today, outflows_today, statement } = response.data;
       setTotalInAccount(total_in_account);
       setTotalInflows(total_inflows);
@@ -64,22 +65,52 @@ export function Extract() {
     }
   };
 
-  const handleOpenModal = () => {
-    setFilter(true);
-  };
 
-  const handleCloseModal = () => {
-    setFilter(false);
-  };
 
   useEffect(() => {
     fetchData();
-  }, [state]); 
+  }, []);
+
+
+  const handleRemoveFilter = (filterKey: string) => {
+    localStorage.removeItem(filterKey);
+    switch (filterKey) {
+      case '@extractStartDate':
+        setStartDate('');
+        break;
+      case '@extractEndDate':
+        setEndDate('');
+        break;
+    }
+    fetchData();
+  };
+
+
+  const activeFilters = [
+    localStorage.getItem('@extractStartDate') && localStorage.getItem('@extractEndDate') && {
+      title: 'Data',
+      onClick: () => {
+        handleRemoveFilter('@extractStartDate');
+        handleRemoveFilter('@extractEndDate');
+        setStartDate('');
+        setEndDate('');
+      }
+    }
+  ].filter((filter): filter is { title: string; onClick: () => void } => Boolean(filter));
+
+  const handleSaveToLocalStorage = async () => {
+    if (startDate) localStorage.setItem('@extractStartDate', startDate)
+    if (endDate) localStorage.setItem('@extractEndDate', endDate)
+
+      fetchData()
+  }
+
+  if(loading) {
+    return <Loading />
+  }
 
   return (
-    <>
-      <ModalExtract onClose={handleCloseModal} visible={filter} />
-      {loading ? <Loading /> :
+
         <>
           <S.Container>
             <HeaderExtract />
@@ -91,11 +122,40 @@ export function Extract() {
               <CardInfo  label='Entradas hoje' value={inflowsToday}/>
               <CardInfo  label='Saídas hoje' value={outflowsToday}/>
             </S.ContainerCard>
-         
+
             <S.ContainerButton>
-           <TotalBtn total='' />.  
-              {state ? <EditableButton /> : ''}
-              <BtnFilter onClick={handleOpenModal} />
+            <BtnFilterModal
+  onClick={handleSaveToLocalStorage}
+  disabled={
+    (!startDate || !endDate || endDate <= startDate)
+  }
+          >
+
+              <CustomInput
+                colorInputDefault={tenantData.primary_color_identity}
+                colorInputSuccess={tenantData.secondary_color_identity}
+                type="date"
+                label="Data inicial"
+                value={startDate}
+                hasError={!!endDate && startDate > endDate}
+                onChange={event => setStartDate(event.target.value)}
+              />
+              <CustomInput
+                colorInputDefault={tenantData.primary_color_identity}
+                colorInputSuccess={tenantData.secondary_color_identity}
+                type="date"
+                label="Data final"
+                value={endDate}
+                hasError={!!startDate && (endDate <= startDate || !endDate)}
+                onChange={event => setEndDate(event.target.value)}
+              />
+
+          </BtnFilterModal>
+
+          {activeFilters.length > 0 && (
+              <TagFilter filters={activeFilters} />
+            )}
+
             </S.ContainerButton>
 
             <TableExtract statement={statement} />
@@ -106,7 +166,5 @@ export function Extract() {
             </S.ContainerCardsMobile>
           </S.Container>
         </>
-      }
-    </>
   );
 }
