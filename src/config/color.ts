0d27c1136b722995f6fac5c9daa6c1f86470a4a1
Joanderson <jhonny_040996@hotmail.com>
2024-06-17 08:23:16 -0300

    feat: teste

--- src/config/color.ts
+++ src/config/color.ts
@@ -3,16 +3,8 @@
 
 const currentHost = window.location.hostname;
 const segment = currentHost.split('.')[1];
-// export const baseURL = `https://api-${segment}-confrapag.stalopay.com.br/`;
+export const baseURL = `https://api-${segment}-confrapag.stalopay.com.br/`;
 
-export const baseURL = 'https://api-pagueassim-confrapag.stalopay.com.br/'
+// export const baseURL = 'https://api-pagueassim-confrapag.stalopay.com.br/'
 
-
-
-// xport const baseURL = 'https://api-pagueassim.stalopay.com.br/'
-
-// const currentURL = window.location.hostname;
-// const segment = currentURL.split('.')[0];
-
-// export const baseURL = `https://api-${segment}.stalopay.com.br/`;
 
