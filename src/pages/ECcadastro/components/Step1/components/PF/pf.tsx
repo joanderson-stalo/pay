37838d0fa9e6b9712c9df0392abccd5ce213c9d3
RodrigoHenrique <rodrigo_pbro@hotmail.com>
2024-06-28 16:30:56 -0300

    fix: step edit estabelecimento

--- src/pages/ECcadastro/components/Step1/components/PF/pf.tsx
+++ src/pages/ECcadastro/components/Step1/components/PF/pf.tsx
@@ -73,7 +73,7 @@ export function PF({ Avançar, BPF, BPJ }: IStep1) {
         if (data_nascimento) setValue('NascimentoSocio', data_nascimento)
       }
     } catch (error) {
-    
+
     } finally {
       setIsLoading(false)
     }
