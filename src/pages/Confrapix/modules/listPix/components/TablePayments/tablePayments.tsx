c6f9889d1ecfb65d07174f239099119008a0f147
Joanderson <jhonny_040996@hotmail.com>
2024-07-04 14:15:25 -0300

    fix

--- src/pages/Confrapix/modules/listPix/components/TablePayments/tablePayments.tsx
+++ src/pages/Confrapix/modules/listPix/components/TablePayments/tablePayments.tsx
@@ -15,15 +15,11 @@ export interface RowDataPayments {
   customer_name: string;
 }
 
-type SortField = keyof RowDataPayments;
-
 interface TabelaProps {
   rows: RowDataPayments[];
 }
 
 export function TablePayments({ rows }: TabelaProps) {
-  const [sortField, setSortField] = useState<SortField>('created_at');
-  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
   const navigate = useNavigate();
   const { setSelectedPaymentID } = usePaymentID();
 
@@ -48,29 +44,6 @@ export function TablePayments({ rows }: TabelaProps) {
     returned: 'Pagamento recusado'
   };
 
-  const handleSort = (field: SortField) => {
-    if (field === sortField) {
-      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
-    } else {
-      setSortField(field);
-      setSortDirection('asc');
-    }
-  };
-
-  const sortedRows = [...rows].sort((a, b) => {
-    let comparison = 0;
-
-    if (sortField === 'created_at') {
-      comparison = new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
-    } else if (sortField === 'id' || sortField === 'amount_nf') {
-      comparison = Number(a[sortField]) - Number(b[sortField]);
-    } else {
-      comparison = a[sortField].localeCompare(b[sortField]);
-    }
-
-    return sortDirection === 'asc' ? comparison : -comparison;
-  });
-
   const handleViewMoreClick = async (id: number, status: string) => {
     await new Promise(resolve => setTimeout(resolve, 20));
     setSelectedPaymentID(id.toString());
@@ -81,47 +54,44 @@ export function TablePayments({ rows }: TabelaProps) {
     }
   };
 
-
   return (
     <S.Table>
       <thead>
         <tr>
-          <S.TableHeader style={{ cursor: 'pointer' }} onClick={() => handleSort('id')}>
+          <S.TableHeader>
             ID
             <S.TooltipIcon data-tooltip="Identificação única da transação">
               <Info />
             </S.TooltipIcon>
           </S.TableHeader>
-          <S.TableHeader style={{ cursor: 'pointer' }} onClick={() => handleSort('receiver_tranding_name')}>
+          <S.TableHeader>
             Nome do cliente
           </S.TableHeader>
-          <S.TableHeader style={{ cursor: 'pointer' }} onClick={() => handleSort('created_at')}>
+          <S.TableHeader>
             Data da transação
           </S.TableHeader>
-          <S.TableHeader style={{ cursor: 'pointer' }} onClick={() => handleSort('status')}>
-          Valor
+          <S.TableHeader>
+            Valor
             <S.TooltipIcon data-tooltip="Status atual do pagamento">
               <Info size={16} />
             </S.TooltipIcon>
           </S.TableHeader>
           <S.TableHeader>Situação</S.TableHeader>
-                <S.TableHeader></S.TableHeader>
+          <S.TableHeader></S.TableHeader>
         </tr>
       </thead>
       <tbody>
-        {sortedRows.map((row, index) => (
+        {rows.map((row, index) => (
           <tr key={index}>
             <S.TableData>{row.id}</S.TableData>
             <S.TableData>{checkEmpty(row.customer_name)}</S.TableData>
             <S.TableData>{formatDateBR(row.created_at)}</S.TableData>
-            <S.TableData>{formatDateBR(row.created_at)}</S.TableData>
+            <S.TableData>{formatCurrencyBRL(row.amount_nf)}</S.TableData>
             <S.TableData>
               <S.StatusTableData status={statusMap[row.status]}>
                 {statusMap[row.status]}
               </S.StatusTableData>
             </S.TableData>
-
-
             <S.TableData>
               <S.Button status={row.status} onClick={() => handleViewMoreClick(row.id, row.status)}>
                 {row.status === 'returned' ? 'Atualizar pagamento' : 'Detalhes'}
